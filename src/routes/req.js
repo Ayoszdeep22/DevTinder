440f55c465458dd765f34f231f515f094198c997
Ayoszdeep22 <ayoszdeepmishra2004@gmail.com>
2025-08-28 14:25:16 +0530

    created-reveiw-api

--- src/routes/req.js
+++ src/routes/req.js
@@ -62,4 +62,50 @@ request.post("/request/send/:status/:touserId",profileauth,async(req,res)=>{
     }
     
 });
+
+
+request.post("/request/review/:status/:requestId",profileauth, async(req,res)=>{
+    try {
+        const LoggedInUser=req.user;
+        const{status,requestId}=req.params;
+        const isAllowedStatus=["accepted","rejected"];
+        if(!isAllowedStatus.includes(status)){
+           if (!isAllowedStatus.includes(status)) {
+  return res.status(400).json({ message: "Invalid status. Use 'accepted' or 'rejected'." });
+}
+
+        };
+        const connectionRequests=await connectionRequest.findOne({
+            _id:requestId,// checking wheteher the reuqest is valid or not 
+            RecieverConnection:LoggedInUser._id,
+            status:"interested",
+
+        });
+        if(!connectionRequests){
+            return res.status(404).json({message:"connection request is not valid "});
+        };
+        connectionRequests.status=status;
+        const data=await connectionRequests.save(); 
+        res.status(200).json({ message: "successful data", data });
+
+
+
+
+
+
+
+
+
+        
+    }  catch(error) {
+  console.error("REVIEW ERR:", error);
+  return res.status(500).json({ message: "Unable to review request", error: error.message });
+}
+
+
+
+
+});
+
+
 module.exports=request;
